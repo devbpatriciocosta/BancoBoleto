227b3d7f623e440405d15c4e1a9d4d6b2750e6ee
devBruno <bpatriciocosta@gmail.com>
2023-01-02 18:50:40 -0300

    React Config

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+    primary: '#FEC350',
+    secondary: '#E8BB00',
+    background: '#000000',
+    secondaryBackground: '#FEC350',
+    lineBar: '#1E1E1E',
+    postBackgroundColor: '#d1d1d1'
+  }
+  
+  export default theme
