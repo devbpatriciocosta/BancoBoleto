fd31e445e1e82d4fdf05135089ef75344d3ef07f
devBruno <bpatriciocosta@gmail.com>
2023-01-03 19:10:10 -0300

    Adjustment in theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,8 @@
 const theme = {
-    primary: '#FEC350',
-    secondary: '#E8BB00',
-    background: '#000000',
-    secondaryBackground: '#FEC350',
-    lineBar: '#1E1E1E',
-    postBackgroundColor: '#d1d1d1'
-  }
-  
-  export default theme
+  primary: '#92F981',
+  secondary: '#FFFFFF',
+  black: '#000000',
+  grey: '#d1d1d1'
+}
+
+export default theme
