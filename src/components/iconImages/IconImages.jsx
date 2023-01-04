85ff0e5043b85a85a15452f15813dd67939eeda1
devBruno <bpatriciocosta@gmail.com>
2023-01-04 16:14:05 -0300

    Redone iconimages and using

--- src/components/iconImages/IconImages.jsx
+++ src/components/iconImages/IconImages.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 export default function IconImages({ imageName, type }) {
-  // eslint-disable-next-line jsx-a11y/alt-text
   return <img src={`images/${imageName}.${type}`} />
 }
