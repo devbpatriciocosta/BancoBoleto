9f384df4a763b6a1a7fabf713994e6878eb52792
devBruno <bpatriciocosta@gmail.com>
2023-01-04 16:14:26 -0300

    Creating Button to open account and register

---
+++ src/components/buttons/OpenCreateButton.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const StyledOpenCreateButton = styled.div`
+  cursor: pointer;
+  width: 188px;
+  height: 35px;
+  border-radius: 20px;
+  background-color: #92f981;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-in-out;
+
+  :hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+    background-color: #3e8e41;
+  }
+
+  :active {
+    box-shadow: 0 5px #666;
+    transform: translateY(2px);
+  }
+`
+
+export default function OpenCreateButton() {
+  return <StyledOpenCreateButton>Abra sua conta</StyledOpenCreateButton>
+}
