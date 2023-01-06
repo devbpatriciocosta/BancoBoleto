db46148ae8a18375faa3dc9f900b2b993a9d194c
devBruno <bpatriciocosta@gmail.com>
2023-01-06 16:05:06 -0300

    creating the login page = landing page

---
+++ pages/initialLoginPage.jsx
@@ -0,0 +1,13 @@
+import FrontPage from '../src/components/layout/frontPage/FrontPage'
+import Navbar from '../src/components/layout/navbar/Navbar'
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <FrontPage />
+    </>
+  )
+}
+
+export default HomePage
