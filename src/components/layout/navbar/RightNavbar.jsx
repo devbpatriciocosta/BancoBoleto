c4ebb550258bcbfc73084cc86ce7491d0aec861b
devBruno <bpatriciocosta@gmail.com>
2023-01-07 16:09:02 -0300

    Link to index troughout the "entrar" button

--- src/components/layout/navbar/RightNavbar.jsx
+++ src/components/layout/navbar/RightNavbar.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
+import Link from 'next/link'
 
-import StyledNavbarButton from '../../buttons/StyledNavbarButton'
 import InputAccount from '../../inputs/InputAccount'
 import InputAgency from '../../inputs/InputAgency'
 
@@ -82,6 +82,33 @@ const StyledInputDigit = styled.input`
   }
 `
 
+const StyledNavbarButton = styled.button`
+  width: 58px;
+  height: 22px;
+  border: none;
+  border-radius: 50px;
+  background-color: ${(props) => props.theme.secondary};
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+
+  :hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+    background-color: ${(props) => props.theme.primary};
+  }
+
+  :active {
+    background-color: #3e8e41;
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: black;
+  }
+`
+
 export default function RightNavbar() {
   return (
     <StyledUl>
@@ -91,7 +118,9 @@ export default function RightNavbar() {
       <InputAgency />
       <InputAccount />
       <StyledInputDigit></StyledInputDigit>
-      <StyledNavbarButton />
+      <StyledNavbarButton>
+        <Link href="/">Entrar</Link>
+      </StyledNavbarButton>
     </StyledUl>
   )
 }
