9cad5c6ca0106d626c70ff1d3b758b4d6b48af47
devBruno <bpatriciocosta@gmail.com>
2023-01-08 19:06:39 -0300

    Index adjusment to building app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,17 @@
+import styled from 'styled-components'
+
+const StyledBody = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.primary};
+  height: 100vh;
+`
+
 export default function BankApplication() {
-  return <h1>Esse é o banco do seu boleto - Estamos em construção!</h1>
+  return (
+    <StyledBody>
+      <h1>Esse é o banco do seu boleto - Estamos em construção!</h1>
+    </StyledBody>
+  )
 }
