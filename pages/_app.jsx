b0514445e6fc34b3aab2666061583ffee9924da7
devBruno <bpatriciocosta@gmail.com>
2023-01-08 18:52:26 -0300

    App and document adjustment + font style

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
 
