9b185764d519979a1a49e9df73b06e399019a486
devBruno <bpatriciocosta@gmail.com>
2023-01-09 12:31:25 -0300

    Adding Header and Resume Section on Index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,11 @@
-import styled from 'styled-components'
-
-const StyledBody = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: ${(props) => props.theme.primary};
-  height: 100vh;
-`
+import Header from '../src/components/header/Header'
+import Resume from '../src/components/resume/Resume'
 
 export default function BankApplication() {
   return (
-    <StyledBody>
-      <h1>Esse é o banco do seu boleto - Estamos em construção!</h1>
-    </StyledBody>
+    <>
+      <Header />
+      <Resume />
+    </>
   )
 }
