735769b06987699e231f7924e4ea0407a7400997
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-15 19:01:39 -0300

    Input Improvements to make resusable

--- src/components/inputs/RegisterInput.jsx
+++ src/components/inputs/RegisterInput.jsx
@@ -1,18 +1,54 @@
 import styled from 'styled-components'
 
+const RegisterInputContainer = styled.div`
+  min-width: 70%;
+`
+
+const RegisterLabel = styled.p`
+  font-weight: bold;
+  font-size: 14px;
+  margin-bottom: 5px;
+`
+
 const StyledRegisterInput = styled.input`
-  width: 200px;
-  height: 35px;
+  width: 100%;
+  border: 1px solid ${(props) => props.theme.primary};
   border-radius: 10px;
-  padding-left: 10px;
+  background-color: ${(props) => props.theme.grey};
+  padding: 15px 20px;
+  box-sizing: border-box;
+
   transition: 0.5s ease-in-out;
-  background-color: ${(props) => props.theme.secondary};
 
   :hover {
-    box-shadow: 0 0 0 2px black;
+    box-shadow: 0 0 0 2px #60f101;
+  }
+
+  ${(props) => props.error && `border: 2px solid ${(props) => props.theme.error}`}
+
+  &:focus {
+    outline: none;
   }
 `
 
-export default function RegisterInput() {
-  return <StyledRegisterInput />
+const ErrorLabel = styled.span`
+  color: ${(props) => props.theme.error};
+  font-weight: bold;
+  font-size: 14px;
+`
+
+const errorMessage = {
+  'string.empty': 'Este campo é obrigatório',
+  'string.email': 'Por favor, digite um e-mail válido'
 }
+
+const RegisterInput = ({ label, placeholder }) => {
+  return (
+    <RegisterInputContainer>
+      <RegisterLabel>{label}</RegisterLabel>
+      <StyledRegisterInput placeholder={placeholder} />
+    </RegisterInputContainer>
+  )
+}
+
+export default RegisterInput
