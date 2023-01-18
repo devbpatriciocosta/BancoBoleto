e77edcd036c5e4f1f0ef6ce263a285d24b5674ce
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-18 15:49:40 -0300

    Criado middlewares para o DB

--- pages/api/user/signin.js
+++ pages/api/user/signin.js
@@ -1,5 +1,6 @@
-import connect from 'next-connect'
 import Joi from 'joi'
+
+import createHandler from '../../../lib/middlewares/nextConnect'
 
 import validate from '../../../lib/middlewares/validation'
 
@@ -13,7 +14,9 @@ const postSchema = Joi.object({
   password: Joi.string().required().max(50).min(6)
 })
 
-const signIn = connect().post(validate({ body: postSchema }), (req, res) => {
+const signIn = createHandler()
+
+signIn.post(validate({ body: postSchema }), (req, res) => {
   signInUser(req.body)
   res.status(200).json({ teste: 'ok' })
 })
