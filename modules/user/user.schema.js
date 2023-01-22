028e2909b33005e84bd4ca958d9d2324e96092f4
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-22 14:17:59 -0300

    Error message to user

--- modules/user/user.schema.js
+++ modules/user/user.schema.js
@@ -1,12 +1,27 @@
 import Joi from 'joi'
 
 export const sigInSchema = Joi.object({
-  firstName: Joi.string().required().max(50),
-  lastName: Joi.string().required().max(50),
-  user: Joi.string().required().max(30),
+  firstName: Joi.string()
+    .required()
+    .max(50)
+    .message('O campo "Nome" pode ter no máximo {{#limit}} caracteres'),
+  lastName: Joi.string()
+    .required()
+    .max(50)
+    .message('O campo "Sobrenome" pode ter no máximo {{#limit}} caracteres'),
+  user: Joi.string()
+    .required()
+    .max(30)
+    .message('O campo "Usuário" pode ter no máximo {{#limit}} caracteres'),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required()
-    .max(100),
-  password: Joi.string().required().max(50).min(6)
+    .max(100)
+    .message('O campo "E-mail" pode ter no máximo {{#limit}} caracteres'),
+  password: Joi.string()
+    .required()
+    .max(50)
+    .message('O campo "Senha" pode ter no máximo {{#limit}} caracteres')
+    .min(6)
+    .message('O campo "Senha" pode ter no mínimo {{#limit}} caracteres')
 })
