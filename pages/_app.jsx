d512ffcd9088a4d4300fd4218cfa751cd90275c8
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-23 19:46:26 -0300

    Font Adjustments

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,10 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  body {
+    font-family: 'Unbounded', cursive;
   }
 `
 
