3bb4c4937720dda5e3a880fefd2f7056d50be1aa
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-24 12:44:19 -0300

    Re doing the navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,38 +1,66 @@
 import styled from 'styled-components'
 
-import LeftNavbar from './LeftNavbar'
-import Burguer from './Burger'
+import { GiBank } from 'react-icons/gi'
 
 const StyledNavbar = styled.nav`
   display: flex;
   align-items: center;
-  width: 85%;
-  height: 100px;
+  justify-content: center;
+  flex-direction: column;
+  color: ${(props) => props.theme.fontColor};
+  width: 100%;
+  margin-top: 10px;
   position: fixed;
-  background-color: none;
+  font-size: 40px;
 
-  @media (max-width: 875px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 70px;
-    padding: 10px;
-    gap: 10px;
+  cursor: grab;
+
+  h1 {
+    transition: 0.2s ease-in-out;
+    color: ${(props) => props.theme.fontColor};
+    font-size: 50px;
+    font-weight: bold;
+    letter-spacing: 7px;
+    cursor: pointer;
   }
 
-  @media (max-width: 360px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 80px;
+  h1 span {
+    transition: 0.2s linear;
+  }
+
+  h1:hover span:nth-child(1) {
+    margin-right: 5px;
+    margin-top: 10px;
+  }
+
+  h1:hover span:nth-child(2):before {
+    content: 'do Seu ';
+  }
+
+  h1:hover span:nth-child(2) {
+    margin-left: 30px;
+  }
+
+  h1:hover span {
+    color: #fff;
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #fff;
+  }
+
+  h2 {
+    animation: lights;
   }
 `
 
 export default function Navbar() {
   return (
     <StyledNavbar>
-      <LeftNavbar />
-      <Burguer />
+      <h1>
+        <span>Banco</span>
+        <span>Boleta</span>
+      </h1>
+      <h2>
+        <GiBank />
+      </h2>
     </StyledNavbar>
   )
 }
