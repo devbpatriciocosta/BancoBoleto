8c2b2810e101e92556fe35076b073288ec663677
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-24 12:44:09 -0300

    Adjustments on Login Page

--- src/components/componentLoginPage/ComponentLoginPage.jsx
+++ src/components/componentLoginPage/ComponentLoginPage.jsx
@@ -3,12 +3,22 @@ import styled from 'styled-components'
 import LoginCard from '../../components/layout/cards/LoginCard'
 
 const FrontPageContainer = styled.div`
-  background-image: url('/bgSeuSeuBoleta.png');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100%;
+  background: linear-gradient(37deg, #0172af, #74fabd, #8776d4, #932ebb);
+  background-size: 300% 300%;
+  animation: gradient-animation 6s ease infinite;
   height: 100vh;
+
+  @keyframes gradient-animation {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `
 
 const CardContainer = styled.div`
