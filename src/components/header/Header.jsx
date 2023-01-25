c4cdfe572d083daab6ce8ccd78b5e11ad13e0095
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-25 10:09:03 -0300

    Controle financeiro sign key frames

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,54 @@ const TitleHeader = styled.div`
   font-weight: bold;
   color: ${(props) => props.theme.fontColor};
 
+  transition: 0.3s ease-in-out;
+
+  :hover {
+    box-shadow: 0 0 0 5px hsla(620, 100%, 50%, 0.5);
+  }
+
+  h1 {
+    transition: 0.4s ease-in-out;
+    font-size: 3.5rem;
+    font-weight: 500;
+    animation: lights 5s 750ms linear infinite;
+  }
+
+  @keyframes lights {
+    0% {
+      color: hsl(230, 100%, 95%);
+      text-shadow: 0 0 1em hsla(600, 100%, 50%, 0.2), 0 0 0.125em hsla(600, 100%, 60%, 0.3),
+        -1em -0.125em 0.5em hsla(40, 100%, 60%, 0), 1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
+    }
+
+    30% {
+      color: hsl(230, 80%, 90%);
+      text-shadow: 0 0 1em hsla(620, 100%, 50%, 0.5), 0 0 0.125em hsla(620, 100%, 60%, 0.5),
+        -0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
+        0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
+    }
+
+    40% {
+      color: hsl(230, 40%, 80%);
+      text-shadow: 0 0 1em hsla(620, 100%, 50%, 0.5), 0 0 0.125em hsla(620, 100%, 60%, 0.5),
+        -0.25em -0.125em 0.125em hsla(40, 100%, 60%, 0.2),
+        0.25em 0.125em 0.125em hsla(200, 100%, 60%, 0.4);
+    }
+
+    70% {
+      color: hsl(230, 80%, 90%);
+      text-shadow: 0 0 1em hsla(620, 100%, 50%, 0.5), 0 0 0.125em hsla(620, 100%, 60%, 0.5),
+        0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
+        -0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
+    }
+
+    100% {
+      color: hsl(230, 100%, 95%);
+      text-shadow: 0 0 1em hsla(600, 100%, 50%, 0.2), 0 0 0.125em hsla(620, 100%, 60%, 0.3),
+        1em -0.125em 0.5em hsla(40, 100%, 60%, 0), -1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
+    }
+  }
+
   @keyframes gradient-animation {
     0% {
       background-position: 0% 50%;
@@ -27,7 +75,9 @@ const TitleHeader = styled.div`
 export default function Header() {
   return (
     <>
-      <TitleHeader>Controle Financeiro</TitleHeader>
+      <TitleHeader>
+        <h1>Controle Financeiro</h1>
+      </TitleHeader>
     </>
   )
 }
