b773596ba0b404e8b17447946796ed19b09d4379
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-31 15:44:02 -0300

    Theme colors Adjustments

--- src/theme.js
+++ src/theme.js
@@ -2,11 +2,12 @@ const theme = {
   primary: '#92f981',
   secondary: '#ffffff',
   terciary: '#3e8e41',
-  black: '#000000',
+  black: '#3a3939',
   grey: '#e9e9e9',
   error: 'red',
   fontColor: '#f5f5f5',
-  backGroundCardColor: '#e1e1e1'
+  backGroundCardColor: '#e1e1e1',
+  borderColor: 'hsla(620, 100%, 50%, 0.5)'
 }
 
 export default theme
